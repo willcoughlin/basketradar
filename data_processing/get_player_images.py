0840405dcb44534861619400999cfd44baa668e1
Liam O'Donnell <133874547+liam-odonnell9@users.noreply.github.com>
2024-10-28 11:13:26 -0500

    added player images link

---
+++ data_processing/get_player_images.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+## loading data
+df = pd.read_csv('cleaned_final_dataset.csv')
+
+# unique players
+unique_players = df['player'].unique()
+unique_df = pd.DataFrame(unique_players, columns=['player'])
+
+# create scrape_name
+def create_scrape_name(player):
+    parts = player.replace('.', '').lower().split()
+    if len(parts) == 2:
+        first, last = parts
+        scrape_name = last[:5] + first[:2] + "01" 
+    else:
+        scrape_name = parts[0][:5] + "01" 
+    return scrape_name
+
+unique_df['scrape_name'] = unique_df['player'].apply(create_scrape_name)
+
+# generate link
+def get_image_link(scrape_name):
+    link = f"https://www.basketball-reference.com/req/202106291/images/headshots/{scrape_name}.jpg"
+    return link
+unique_df['player_image_link'] = unique_df['scrape_name'].apply(get_image_link)
+
+unique_df.to_csv('player_images.csv')
